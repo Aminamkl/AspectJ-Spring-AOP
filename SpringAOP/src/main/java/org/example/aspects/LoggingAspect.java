2b72539d5fd934946a89a82250ff0c20d8e4ba4d
Aminamkl <Maakoulamina@gmail.com>
2022-12-22 18:11:01 +0100

    LoggingAspect

--- SpringAOP/src/main/java/org/example/aspects/LoggingAspect.java
+++ SpringAOP/src/main/java/org/example/aspects/LoggingAspect.java
@@ -7,24 +7,26 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.logging.Logger;
 
 @Component
 @Aspect
 @EnableAspectJAutoProxy
 public class LoggingAspect {
+    Logger logger=Logger.getLogger(LoggingAspect.class.getName());
     @Around("@annotation(org.example.aspects.Log)")
     public Object log(ProceedingJoinPoint joinPoint) {
         Object result=null;
         Date d1 = new Date();
-        System.out.println("Before ...." + d1);
+        logger.info("Before ...." + d1);
         try {
             result = joinPoint.proceed();
         } catch (Throwable throwable) {
             throwable.printStackTrace();
         }
         Date d2 = new Date();
-        System.out.println("After .... " + d2);
-        System.out.println("Execution Duration : "+(d2.getTime()-d1.getTime()));
+        logger.info("After .... " + d2);
+        logger.info("Execution Duration : "+(d2.getTime()-d1.getTime()));
         return result;
     }
 }
